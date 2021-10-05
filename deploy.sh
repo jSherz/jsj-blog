6c9ade26e4e3a858dbcb4f1dc127602724fc9c74
James Sherwood-Jones <james.sherwood-jones@and.digital>
2021-10-05 11:50:55 +0100

    use new bucket

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,9 @@ aws s3 sync \
     --delete \
     --metadata-directive REPLACE \
     --cache-control "max-age=86400" \
-    _site/ s3://jsherz.com/
+    _site/ s3://jsj-prod-jsherz-com-website/
 
 DISTRIBUTION_ID=$(aws cloudfront list-distributions | jq --raw-output '.DistributionList.Items | map(select((.Aliases.Quantity >= 1) and (.Aliases.Items | contains(["jsherz.com"])))) | .[0].Id')
 
+export PAGER=""
 aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --path '/*'
